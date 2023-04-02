574ae93ccdeaba40ff6cf261abd5ce338f58446b
Candela Utrero <curh1@gcloud.ua.es>
2023-04-02 14:12:33 +0200

    estructura base

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'sort.lab',
+  appName: 'sort-lab',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
