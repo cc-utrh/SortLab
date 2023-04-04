4383ff994e527a8c0b28036b5d75257d50a44339
Candela Utrero <curh1@gcloud.ua.es>
2023-04-04 09:19:42 +0200

    cargo modelo desde la aplicación

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import * as tf from '@tensorflow/tfjs';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,20 @@ import { IonicModule } from '@ionic/angular';
   standalone: true,
   imports: [IonicModule],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  public modelo: any
+  public titulo = "Prueba carga modelo";
+  public prediccion = "Ninguna todavia";
+
   constructor() {}
+
+  ngOnInit(): void {
+    this.cargarModelo();
+  }
+
+  async cargarModelo(){
+    this.modelo = await tf.loadGraphModel('../assets/material_model/model.json');
+    this.titulo="Modelo cargado!";
+  }
+
 }
